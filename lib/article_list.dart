e7aaa7178fe629887c81ccff50ffdee0ae0d723d
chuxu <chuxu@chuxudeMacBook-Pro-2.local>
2020-06-08 16:28:56 +0800

    构造列表item

--- lib/article_list.dart
+++ lib/article_list.dart
@@ -25,15 +25,53 @@ class _ArticleListState extends State<ArticleList> {
                   arguments: '${listData[index]}');
             },
             child: Container(
-              height: 50,
-              color: Colors.amber[colorCodes[index]],
-              child: Center(
-                child: Text('${listData[index]}'),
+              height: 100,
+              // color: Colors.amber[colorCodes[index]],
+              child: Row(
+                children: <Widget>[
+                  buildLeftRow(context),
+                  buildRightRow(context)
+                ],
               ),
             ),
           );
         },
       ),
     );
+  }
+
+  Widget buildLeftRow(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(4),
+        child: Image.network(
+            'https://cw1.tw/CW/images/article/201803/article-5ab3573ec0d41.jpg'));
+  }
+
+  Widget buildRightRow(BuildContext context) {
+    return Expanded(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 4.0),
+          child: Text(
+            '我是标题，哈哈哈哈哈哈哈哈',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+          child: Text(
+            '我是文章的部分内容我是文章的部分内容我是文章的部分内容我是文章的部分内容我是文章的部分内容我是文章的部分内容我是文章的部分内容我是文章的部分内容',
+            style: TextStyle(color: Color(0xFF8E8D92), fontSize: 16),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        )
+      ],
+    ));
   }
 }
